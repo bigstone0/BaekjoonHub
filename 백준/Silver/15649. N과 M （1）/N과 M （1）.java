200d2ac178405518b2e0e86e54cb5e5c01a89330
bigstone0 <110443412+bigstone0@users.noreply.github.com>
2024-02-05 03:28:15 +0900

    [Silver III] Title: N과 M (1), Time: 2184 ms, Memory: 50612 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static int[] num;
+    static boolean[] check;
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(bf.readLine());
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        num=new int[M];
+        check=new boolean[N];
+
+        for(int i=0;i<N;i++){
+            check[i]=true;
+            num[0]=i+1;
+            dfs(i+1,1);
+            check[i]=false;
+        }
+    }
+
+    static void dfs(int index,int count){
+        if(count==M) {
+            for(int i=0;i<M;i++) System.out.print(num[i]+" ");
+            System.out.println();
+            return;
+        }
+
+        for(int i=0;i<N;i++){
+            if(check[i]) continue;
+            check[i]=true;
+            num[count]=i+1;
+            dfs(i,count+1);
+            check[i]=false;
+        }
+    }
+}
